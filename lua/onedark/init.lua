a3961ed0bc2ab38eb34d5e51a4f78dc4e260a40e
olimorris <olimorris@users.noreply.github.com>
2021-09-13 18:48:15 +0100

    fix: improve how the color scheme is loaded at init

--- lua/onedark/init.lua
+++ lua/onedark/init.lua
@@ -15,4 +15,22 @@ function M.load(name)
   utils.load(theme)
 end
 
+-- Use a command to load a specific theme color
+function M.load_complete(lead, _, _)
+  local colors = require("onedark.colors").themes
+
+  if lead == "" then
+    return colors
+  end
+
+  local completion_list = {}
+  for _, name in pairs(colors) do
+    if vim.startswith(name, lead) then
+      table.insert(completion_list, name)
+    end
+  end
+
+  return completion_list
+end
+
 return M
