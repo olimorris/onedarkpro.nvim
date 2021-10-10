ea08be952f4c8c3d028b79da7eda4ea227d90d3a
olimorris <olimorris@users.noreply.github.com>
2021-10-10 15:43:41 +0100

    fix: allow additional colors to be overridden
    The theme has two color palettes; core and additional. The latter is based off of core colors and is used for items such as indent markings, selections, menus etc.
    
    With the previous implementation, if any of the core colors were overriden, the additional colors would not be updated to reflected the new color. This was because they were hard coded with hex values.
    
    In this fix, we use the core color variables instead of hex's.
    
    Signed-off-by: olimorris <olimorris@users.noreply.github.com>

--- lua/onedark/colors/onedark.lua
+++ lua/onedark/colors/onedark.lua
@@ -2,11 +2,12 @@ local utils = require("onedark.utils")
 
 local M = {}
 
-function M.init()
+function M.load(config)
+	config = config or require("onedark.config").options
+
 	local colors = {
 		name = "onedark",
 
-		-- Core colors
 		bg = "#1E1E1E",
 		fg = "#abb2bf",
 		red = "#e06c75",
@@ -19,24 +20,17 @@ function M.init()
 		black = "#1E1E1E",
 		gray = "#4c4f55",
 		highlight = "#e2be7d",
-
-		-- Unique colors that we need
-		comment = utils.lighten("#4c4f55", 0.8), -- based on gray
-		selection = utils.lighten("#1E1E1E", 0.8), -- based on bg
-		color_column = utils.lighten("#1E1E1E", 0.97), -- based on bg
-		indentline = utils.lighten("#1E1E1E", 0.92), -- based on bg
-		menu = utils.lighten("#1E1E1E", 0.85), -- based on bg
-		menu_scroll = utils.lighten("#1E1E1E", 0.90), -- based on bg
-		menu_scroll_thumb = utils.darken("#118dc3", 0.80), -- based on blue
 	}
 
-	return colors
-end
+	-- Additional colors
+	colors.comment = utils.lighten(colors.gray, 0.8)
+	colors.selection = utils.lighten(colors.bg, 0.8)
+	colors.color_column = utils.lighten(colors.bg, 0.97)
+	colors.indentline = utils.lighten(colors.bg, 0.92)
+	colors.menu = utils.lighten(colors.bg, 0.85)
+	colors.menu_scroll = utils.lighten(colors.bg, 0.90)
+	colors.menu_scroll_thumb = utils.darken(colors.blue, 0.80)
 
-function M.load(config)
-	config = config or require("onedark.config").options
-
-	local colors = M.init()
 	utils.color_overrides(colors, config)
 
 	return colors
--- lua/onedark/colors/onelight.lua
+++ lua/onedark/colors/onelight.lua
@@ -2,11 +2,12 @@ local utils = require("onedark.utils")
 
 local M = {}
 
-function M.init()
+function M.load(config)
+	config = config or require("onedark.config").options
+
 	local colors = {
 		name = "onelight",
-
-		-- Core colors
+		
 		bg = "#fafafa",
 		fg = "#6a6a6a",
 		red = "#e05661",
@@ -19,24 +20,17 @@ function M.init()
 		black = "#6a6a6a",
 		gray = "#bebebe",
 		highlight = "#e2be7d",
-
-		-- Unique colors that we need
-		comment = utils.darken("#bebebe", 0.80), -- based on gray
-		selection = utils.darken("#fafafa", 0.90), -- based on bg
-		color_column = utils.darken("#fafafa", 0.98), -- based on bg
-		indentline = utils.darken("#fafafa", 0.93), -- based on bg
-		menu = utils.darken("#fafafa", 0.95), -- based on bg
-		menu_scroll = utils.darken("#fafafa", 0.90), -- based on bg
-		menu_scroll_thumb = utils.lighten("#118dc3", 0.80), -- based on blue
 	}
 
-	return colors
-end
+	-- Additional colors
+	colors.comment = utils.darken(colors.gray, 0.80)
+	colors.selection = utils.darken(colors.bg, 0.90)
+	colors.color_column = utils.darken(colors.bg, 0.98)
+	colors.indentline = utils.darken(colors.bg, 0.93)
+	colors.menu = utils.darken(colors.bg, 0.95)
+	colors.menu_scroll = utils.darken(colors.bg, 0.90)
+	colors.menu_scroll_thumb = utils.lighten(colors.blue, 0.80)
 
-function M.load(config)
-	config = config or require("onedark.config").options
-
-	local colors = M.init()
 	utils.color_overrides(colors, config)
 
 	return colors
