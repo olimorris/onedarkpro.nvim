5b20b16be3befda8089f6dbdf2826195f63d753c
Oli Morris <olimorris@users.noreply.github.com>
2022-10-12 18:56:04 +0100

    fix: markdown highlighting

--- lua/onedarkpro/highlights/filetypes/markdown.lua
+++ lua/onedarkpro/highlights/filetypes/markdown.lua
@@ -7,13 +7,13 @@ function M.groups(theme)
     local config = require("onedarkpro.config").config
 
     return {
-        ["@text.literal.markdown"] = { fg = theme.palette.green },
-        ["@text.reference.markdown"] = { fg = theme.palette.blue },
+        ["@text.literal.markdown_inline"] = { fg = theme.palette.green },
+        ["@text.reference.markdown_inline"] = { fg = theme.palette.blue },
         ["@text.title.markdown"] = { fg = theme.palette.red, style = config.options.bold },
-        ["@parameter.markdown"] = { fg = theme.palette.fg },
+        ["@parameter.markdown_inline"] = { fg = theme.palette.fg },
         ["@punctuation.special.markdown"] = { fg = theme.palette.red },
-        ["@punctuation.delimiter.markdown"] = { fg = theme.palette.gray },
-        ["@text.uri.markdown"] = { fg = theme.palette.green },
+        ["@punctuation.delimiter.markdown_inline"] = { fg = theme.palette.gray },
+        ["@text.uri.markdown_inline"] = { fg = theme.palette.blue },
     }
 end
 
