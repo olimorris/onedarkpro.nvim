7c5d9b0bfb270242012104e1afa37d191ce4eec4
Oli Morris <olimorris@users.noreply.github.com>
2022-10-17 22:30:19 +0100

    chore: formatting and gitignore

--- examples/php.php
+++ examples/php.php
@@ -11,37 +11,37 @@ namespace OneDarkPro;
  */
 class ThingHelper
 {
-	private $thing = null;
+    private $thing = null;
 
-	/**
-	 * @param string $thing 
-	 * @return void 
-	 */
-	public function __construct(string $thing)
-	{
-		$this->thing = $thing;
-	}
+    /**
+     * @param string $thing
+     * @return void
+     */
+    public function __construct(string $thing)
+    {
+        $this->thing = $thing;
+    }
 
-	/** @return string  */
-	public function getThing(): string
-	{
-		return $this->thing;
-	}
+    /** @return string  */
+    public function getThing(): string
+    {
+        return $this->thing;
+    }
 
-	/**
-	 * Print thing a certain number of times
-	 *
-	 * @param int $times 
-	 * @return void 
-	 */
-	public function printThing(int $times = 1): void
-	{
-		if ($this->getThing() === 'Hello World') {
-			echo 'Be more original';
-		}
+    /**
+     * Print thing a certain number of times
+     *
+     * @param int $times
+     * @return void
+     */
+    public function printThing(int $times = 1): void
+    {
+        if ($this->getThing() === 'Hello World') {
+            echo 'Be more original';
+        }
 
-		echo str_repeat($this->getThing(), $times);
-	}
+        echo str_repeat($this->getThing(), $times);
+    }
 }
 
 // Use the class...
@@ -51,19 +51,19 @@ $thing->printThing(3);
 /**
  * A function
  *
- * @param int $a 
- * @param int $b 
- * @return int 
+ * @param int $a
+ * @param int $b
+ * @return int
  */
 function add(int $a, int $b): int
 {
-	return $a + $b;
+    return $a + $b;
 }
 
 // Some random stuff
 $sum = add(1, 2);
 if ($sum !== 3) {
-	echo 'Go back to school, PHP';
+    echo 'Go back to school, PHP';
 }
 
 
@@ -71,9 +71,9 @@ if ($sum !== 3) {
  * Try/catch
  */
 try {
-	add('NaN', 1);
+    add('NaN', 1);
 } catch (\TypeError $e) {
-	echo 'You done goofed';
+    echo 'You done goofed';
 }
 
 /*
@@ -91,7 +91,7 @@ echo <<<END
  */
 $arr1 = [null, 'hi'];
 $arr2 = [
-	'foo' => 1,
-	'baz' => 2,
+    'foo' => 1,
+    'baz' => 2,
 ];
 array_map(fn ($value) => ++$value, $arr2);
