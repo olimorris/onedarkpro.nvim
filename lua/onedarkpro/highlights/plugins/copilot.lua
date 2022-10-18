55b2a219fd56f1984abf4c64913f32e89c80d890
Oli Morris <olimorris@users.noreply.github.com>
2022-10-18 10:58:47 +0100

    fix: copilot styling

--- lua/onedarkpro/highlights/plugins/copilot.lua
+++ lua/onedarkpro/highlights/plugins/copilot.lua
@@ -7,7 +7,7 @@ function M.groups(theme)
     local config = require("onedarkpro.config").init()
 
     return {
-        CopilotSuggestion = { fg = theme.palette.gray, style = config.styles.italic },
+        CopilotSuggestion = { fg = theme.palette.gray, style = config.options.italic },
     }
 end
 
