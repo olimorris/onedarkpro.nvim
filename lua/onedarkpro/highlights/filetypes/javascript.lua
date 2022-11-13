73df5308d6ff983bdb125020fe15444808e384c9
Oli Morris <olimorris@users.noreply.github.com>
2022-11-13 17:13:21 +0000

    chore: update method.call for javascript

--- lua/onedarkpro/highlights/filetypes/javascript.lua
+++ lua/onedarkpro/highlights/filetypes/javascript.lua
@@ -10,7 +10,7 @@ function M.groups(theme, config)
         ["@keyword.javascript"] = { fg = theme.palette.purple, style = config.styles.keywords },
         ["@keyword.return.javascript"] = { fg = theme.palette.purple },
         ["@method.javascript"] = { fg = theme.palette.blue, style = config.options.bold },
-        ["@method.call.javascript"] = { fg = theme.palette.blue, style = config.options.bold },
+        ["@method.call.javascript"] = { link = "@method.javascript" },
         ["@punctuation.bracket.javascript"] = { fg = theme.palette.yellow },
         ["@variable.javascript"] = { fg = theme.palette.red, style = config.options.italic },
         ["@variable.builtin.javascript"] = { fg = theme.palette.yellow, style = config.options.italic },
