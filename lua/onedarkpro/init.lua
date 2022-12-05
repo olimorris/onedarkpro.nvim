7978f03c108f5f0af9937bd2aae4599e5bde6c60
Oli Morris <olimorris@users.noreply.github.com>
2022-12-05 14:33:15 +0000

    update init

--- lua/onedarkpro/init.lua
+++ lua/onedarkpro/init.lua
@@ -27,6 +27,7 @@ function M.clean()
         cache.clean({ theme = theme })
     end
 
+    -- Remove hash files
     cache.clean({ file = "fingerprint" })
     cache.clean({ file = "user_config_hash" })
 end
@@ -34,8 +35,41 @@ end
 ---Reset the colorscheme to the default values
 ---@return nil
 function M.reset()
+    --TODO: To be implemented
     require("onedarkpro.config").reset()
     require("onedarkpro.override").reset()
+end
+
+---Determine if the current fingerprint is valid, updating it if not
+---@param cache_path string
+---@return boolean
+local function valid_user_config(cache_path)
+    local hash_config_path = file.join_paths(cache_path, "user_config_hash")
+    local current_hash = tostring(config.hash())
+    local stored_hash = tostring(file.read(hash_config_path))
+
+    if not stored_hash or current_hash ~= stored_hash then
+        file.write(hash_config_path, current_hash)
+        return false
+    end
+
+    return true
+end
+
+---Determine if the current fingerprint is valid, updating it if not
+---@param cache_path string
+---@return boolean
+local function valid_fingerprint(cache_path)
+    local fingerprint_path = file.join_paths(cache_path, "fingerprint")
+    local current_fingerprint = require("onedarkpro.fingerprint")
+    local stored_fingerprint = file.read(fingerprint_path)
+
+    if not stored_fingerprint or current_fingerprint ~= stored_fingerprint then
+        file.write(fingerprint_path, current_fingerprint)
+        return false
+    end
+
+    return true
 end
 
 ---Setup the colorscheme
@@ -45,37 +79,18 @@ function M.setup(opts)
     local should_cache = false
     config.setup(opts)
 
-    -- Allow users to bypass the hashing and generate themes everytime
-    if not config.config.caching then
-        M.cache()
-        return
-    end
+    -- Allow users to bypass the hashing and generate colorschemes everytime
+    if not config.config.caching then return M.cache() end
 
     local cache_path, _ = config.get_cached_info()
     file.ensure_dir(cache_path)
 
-    --BUG: Have to run hash twice to get consistent hash
+    --BUG: Have to run hash twice to get a consistent hash
+    --TODO: Investigate why this is happening
     config.hash()
 
-    -- Check the user config hash
-    local hash_config_path = file.join_paths(cache_path, "user_config_hash")
-    local current_hash = tostring(config.hash())
-    local stored_hash = tostring(file.read(hash_config_path))
-
-    if not stored_hash or current_hash ~= stored_hash then
-        should_cache = true
-        file.write(hash_config_path, current_hash)
-    end
-
-    -- Check the colorscheme's fingerprint
-    local fingerprint_path = file.join_paths(cache_path, "fingerprint")
-    local current_fingerprint = require("onedarkpro.fingerprint")
-    local stored_fingerprint = file.read(fingerprint_path)
-
-    if not stored_fingerprint or current_fingerprint ~= stored_fingerprint then
-        should_cache = true
-        file.write(fingerprint_path, current_fingerprint)
-    end
+    if not valid_user_config(cache_path) then should_cache = true end
+    if not valid_fingerprint(cache_path) then should_cache = true end
 
     if should_cache then M.cache() end
 end
@@ -83,10 +98,15 @@ end
 ---Load a theme
 ---@return nil
 function M.load()
-    -- Some users may not call the setup method. In this case we need to load
-    -- the config with the defaults. We also need to determine if the theme
-    -- has been cached and compile and cache for all themes if it hasn't
-    if not config.is_setup then config.setup() end
+    -- Some users may not call the setup method and we account for that
+    if not config.is_setup then
+        config.setup()
+        local cache_path, _ = config.get_cached_info()
+
+        --Non-setup users still get the benefits of caching so we need to check
+        --that the fingerprint is valid and generate new colorschemes if not
+        if not valid_fingerprint(cache_path) then M.cache() end
+    end
 
     local _, cached_theme = config.get_cached_info()
     if not file.exists(cached_theme) then M.cache() end
