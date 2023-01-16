b2138d435d5bfef9dc5dbcbc4b2438f74121c561
Oli Morris <olimorris@users.noreply.github.com>
2023-01-16 12:15:12 +0000

    feat(plugins): add additional gitsigns

--- lua/onedarkpro/highlights/plugins/gitsigns.lua
+++ lua/onedarkpro/highlights/plugins/gitsigns.lua
@@ -5,9 +5,14 @@ local M = {}
 ---@return table
 function M.groups(theme)
     return {
-        GitSignsAdd = { link = "diffAdded" },
-        GitSignsChange = { link = "diffChanged" },
-        GitSignsDelete = { link = "diffRemoved" },
+        GitSignsAdd = { fg = theme.palette.green },
+        GitSignsAddCursorLine = { fg = theme.palette.green, bg = theme.palette.cursorline },
+        GitSignsChange = { fg = theme.palette.yellow },
+        GitSignsChangeCursorLine = { fg = theme.palette.yellow, bg = theme.palette.cursorline },
+        GitSignsDelete = { fg = theme.palette.red },
+        GitSignsDeleteCursorLine = { fg = theme.palette.red, bg = theme.palette.cursorline },
+        GitSignsUntracked = { fg = theme.palette.blue },
+        GitSignsUntrackedCursorLine = { fg = theme.palette.blue, bg = theme.palette.cursorline },
     }
 end
 
