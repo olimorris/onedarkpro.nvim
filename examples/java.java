26afe217dd6409fa0c14b87c82b515c07e06f72d
Oli Morris <olimorris@users.noreply.github.com>
2023-02-26 10:46:32 +0000

    chore: add example java file

---
+++ examples/java.java
@@ -0,0 +1,25 @@
+// importing the File class
+import java.io.File;
+
+class Main {
+  public static void main(String[] args) {
+
+    // create a file object for the current location
+    File file = new File("newFile.txt");
+
+    try {
+
+      // trying to create a file based on the object
+      boolean value = file.createNewFile();
+      if (value) {
+        System.out.println("The new file is created.");
+      }
+      else {
+        System.out.println("The file already exists.");
+      }
+    }
+    catch(Exception e) {
+      e.getStackTrace();
+    }
+  }
+}
