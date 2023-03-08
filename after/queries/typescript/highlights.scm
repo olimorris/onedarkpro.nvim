f9999ca7263771d208a485c306f535a1e112bed6
Oli Morris <olimorris@users.noreply.github.com>
2023-03-08 19:48:26 +0000

    feat: add typescript semantic tokens

--- after/queries/typescript/highlights.scm
+++ after/queries/typescript/highlights.scm
@@ -1,11 +1,4 @@
-;; extends
+; extends
 ("=>" @operator.fat_arrow)
 
-(enum_body
-  (property_identifier) @enum.member (#set! "priority" 150))
-
 (interface_declaration (object_type (property_signature (property_identifier) @property.function (type_annotation (type_identifier) @type) (#eq? @type "FuncType"))))
-
-((identifier) @variable.builtin
- (#vim-match? @variable.builtin "^(arguments|module|console|window|document)$")
- (#set! "priority" 150))
