708a6dba9eb9713b28d1e1def6a8da90fc004888
Oli Morris <olimorris@users.noreply.github.com>
2023-03-09 00:10:54 +0000

    feat: update python highlighting

---
+++ after/queries/python/highlights.scm
@@ -0,0 +1,13 @@
+; extends
+(list_splat_pattern ("*" @operator.splat))
+(dictionary_splat_pattern ("**" @operator.splat))
+
+(import_from_statement) "from" @import_from
+
+((function_definition name: (identifier) @base_constructor)
+(#any-of? @base_constructor "__new__" "__init__"))
+
+((identifier) @keyword (#vim-match? @keyword "^(kwargs|self)$"))
+
+"{" @punctuation.brace
+"}" @punctuation.brace
--- lua/onedarkpro/highlights/filetypes/python.lua
+++ lua/onedarkpro/highlights/filetypes/python.lua
@@ -7,9 +7,15 @@ function M.groups(theme)
     local config = require("onedarkpro.config").config
 
     return {
-        ["@constructor.python"] = { fg = theme.palette.cyan },
+        ["@base_constructor.python"] = { fg = theme.palette.cyan },
         ["@conditional.python"] = { fg = theme.palette.purple, style = config.styles.conditionals },
+        ["@constant.builtin.python"] = { fg = theme.palette.orange, style = config.styles.constants },
+        ["@include.python"] = { fg = theme.palette.fg, style = config.styles.keywords },
+        ["@import_from.python"] = { fg = theme.palette.purple, style = config.styles.keywords },
+        ["@keyword.python"] = { style = config.styles.keywords },
         ["@method.call.python"] = { fg = theme.palette.blue, style = config.styles.methods },
+        ["@operator.splat.python"] = { fg = theme.palette.fg },
+        ["@punctuation.brace.python"] = { fg = theme.palette.purple },
         ["@variable.builtin.python"] = { fg = theme.palette.yellow, style = config.styles.variables },
     }
 end
