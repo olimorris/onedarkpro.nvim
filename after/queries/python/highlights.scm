a250d4a59ed71e1a25f198a619a6da0cd4476a77
Anthony Shi <skyskying93@gmail.com>
2023-03-17 09:03:44 +0800

    Updated after/queries/python/highlights.scm: Refactor
    In the task of assigning @odp.constructor to various python built-in types, instead of querying "assignment right" (which handles only the specific case of assignment statements) we can query "call function." The latter takes care of all cases including built-in constructor calls in for_statements, while_statements, if_statements, argument lists, and even basic parenthesized expression.

--- after/queries/python/highlights.scm
+++ after/queries/python/highlights.scm
@@ -7,8 +7,8 @@
 ((function_definition name: (identifier) @odp.base_constructor)
 (#any-of? @odp.base_constructor "__new__" "__init__"))
 
-(assignment right: (call (identifier) @odp.constructor)
-(#any-of? @odp.constructor "range" "list" "tuple" "dict" "set" "frozenset" "str" "int" "float" "complex" "bytes" "bytearray" "memoryview"))
+(call function: (identifier) @odp.constructor
+(#any-of? @odp.constructor "range" "list" "tuple" "dict" "set" "frozenset" "str" "int" "float" "complex" "bool" "bytes" "bytearray" "memoryview"))
 
 ((identifier) @odp.keyword (#vim-match? @odp.keyword "^(kwargs|self)$"))
 (class_definition "class" @odp.keyword.class)
