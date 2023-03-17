a43138792c23e3f56399666049dd57afb61706ae
Anthony Shi <69449791+anthony-S93@users.noreply.github.com>
2023-03-18 00:20:21 +0800

    Update after/queries/python/highlights.scm (#173)

--- after/queries/python/highlights.scm
+++ after/queries/python/highlights.scm
@@ -8,7 +8,7 @@
 (#any-of? @odp.base_constructor "__new__" "__init__"))
 
 (call function: (identifier) @odp.constructor
-(#any-of? @odp.constructor "range" "list" "tuple" "dict" "set" "frozenset" "str" "int" "float" "complex" "bool" "bytes" "bytearray" "memoryview"))
+(#any-of? @odp.constructor "range" "list" "tuple" "dict" "set" "frozenset" "str" "int" "float" "complex" "bool" "bytes" "bytearray" "memoryview" "type"))
 
 ((identifier) @odp.keyword (#vim-match? @odp.keyword "^(kwargs|self)$"))
 (class_definition "class" @odp.keyword.class)
