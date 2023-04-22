0147f69893496b3df605d64acdf4b66f03b0654e
VPavliashvili <v_pavliashvili@yahoo.com>
2023-04-22 18:48:25 +0400

    feat: improved 'go' colors (#180)
    * feat: 'go' support improved according to https://github.com/olimorris/onedarkpro.nvim/commit/434b67beced0b518804712ab04b8cf4bcf1aed7d#commitcomment-109878000
    
    * fix: styles added

--- lua/onedarkpro/highlights/filetypes/go.lua
+++ lua/onedarkpro/highlights/filetypes/go.lua
@@ -7,12 +7,17 @@ function M.groups(theme)
     local config = require("onedarkpro.config").config
 
     return {
-        ["@constant.go"] = { fg = theme.palette.red },
-        ["@function.call.go"] = { fg = theme.palette.cyan },
-        ["@method.call.go"] = { fg = theme.palette.cyan },
-        ["@operator.go"] = { fg = theme.palette.yellow },
-        ["@type.go"] = { fg = theme.palette.fg },
-        ["@type.builtin.go"] = { fg = theme.palette.purple },
+        ["@constant.go"] = { fg = theme.palette.red, style = config.styles.constant },
+        ["@function.call.go"] = { fg = theme.palette.blue, style = config.styles.functions },
+        ["@method.call.go"] = { fg = theme.palette.blue, style = config.styles.methods },
+        ["@function.builtin.go"] = { fg = theme.palette.cyan, style = config.styles.functions },
+        ["@operator.go"] = { fg = theme.palette.cyan, style = config.styles.operators },
+        ["@type.go"] = { fg = theme.palette.yellow, style = config.styles.types },
+        ["@type.builtin.go"] = { fg = theme.palette.purple, style = config.styles.types },
+        ["@field.go"] = { fg = theme.palette.red, style = config.styles.variables },
+        ["@property.go"] = { fg = theme.palette.red, style = config.styles.variables },
+        ["@variable.go"] = { fg = theme.palette.red, style = config.styles.variables },
+        ["@parameter.go"] = { fg = theme.palette.red, style = config.styles.parameters },
     }
 end
 
