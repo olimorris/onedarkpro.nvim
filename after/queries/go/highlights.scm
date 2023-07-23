bcf768e9148f4e0acd52c8e418dbe6930885239f
Anthony Shi <69449791+anthony-S93@users.noreply.github.com>
2023-07-23 18:02:14 +0800

    Create highlights.scm

---
+++ after/queries/go/highlights.scm
@@ -0,0 +1,3 @@
+; extends
+(import_declaration (import_spec path: (interpreted_string_literal) @odp.import_module))
+(import_declaration (import_spec_list (import_spec path: (interpreted_string_literal) @odp.import_module)))
