d850a4088ca5574b927982209500a858292d3f59
Oli Morris <olimorris@users.noreply.github.com>
2023-12-27 02:04:53 +0000

    feat: update lua highlights

--- lua/onedarkpro/highlights/filetypes/lua.lua
+++ lua/onedarkpro/highlights/filetypes/lua.lua
@@ -11,17 +11,19 @@ function M.groups(theme)
         ["@function.call.lua"] = { fg = theme.palette.blue, style = config.styles.functions },
         ["@field.lua"] = { fg = theme.palette.fg },
         ["@keyword.lua"] = { fg = theme.palette.purple, style = config.styles.keywords },
+        ["@keyword.function.lua"] = { fg = theme.palette.purple, style = config.styles.keywords },
         ["@keyword.operator.lua"] = { fg = theme.palette.fg },
         ["@method.lua"] = { fg = theme.palette.blue, style = config.styles.methods },
         ["@operator.lua"] = { fg = theme.palette.fg, style = config.styles.operators },
         ["@parameter.lua"] = { fg = theme.palette.fg, style = config.styles.parameters },
         ["@punctuation.bracket.lua"] = { fg = theme.palette.orange },
+        ["@punctuation.bracket.luadoc"] = { fg = theme.palette.comment },
 
         ["@odp.function_assignment.lua"] = { link = "@function" },
 
         -- LSP Semantic Tokens
         ["@lsp.typemod.function.defaultLibrary.lua"] = { fg = theme.palette.cyan },
-        ["@lsp.type.class.lua"] = { fg = theme.palette.yellow },
+        ["@lsp.type.class.lua"] = { fg = theme.palette.yellow, style = config.styles.functions },
         ["@lsp.typemod.variable.defaultLibrary.lua"] = { fg = theme.palette.yellow },
     }
 end
