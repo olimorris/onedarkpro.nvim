96f87c39c3f3531ad2cf693dacd0c23221ea00f1
Cuenca Pablo <148780611+cuencapm@users.noreply.github.com>
2024-01-19 10:38:04 -0500

    Adding Lualine center bar transparency option

--- lua/onedarkpro/config.lua
+++ lua/onedarkpro/config.lua
@@ -90,10 +90,8 @@ local defaults = {
         cursorline = false, -- Use cursorline highlighting?
         transparency = false, -- Use a transparent background?
         terminal_colors = true, -- Use the theme's colors for Neovim's :terminal?
+        lualine_transparency = true, -- Center bar transparency?
         highlight_inactive_windows = false, -- When the window is out of focus, change the normal background?
-    },
-    lualine = {
-        transparent = false, -- lualine center bar transparency
     },
 }
 
