13c3244b2520a832989d797d69436df7d27e20b9
Ponlakrit Parnsa-nguard <69962410+nickp-real@users.noreply.github.com>
2024-05-26 02:34:34 +0700

    fix(alacritty): update file to compatible with 0.13 (#238)

--- lua/onedarkpro/extra/alacritty.lua
+++ lua/onedarkpro/extra/alacritty.lua
@@ -1,41 +1,40 @@
 local M = require("onedarkpro.utils.template")
 
-M.filetype = "yaml"
+M.filetype = "toml"
 
 M.template = [[
 # Colors - https://github.com/olimorris/onedarkpro.nvim
-colors:
-  primary:
-    background: '${bg}'
-    foreground: '${fg}'
+[colors.primary]
+background: '${bg}'
+foreground: '${fg}'
 
-  normal:
-    black:      '${black}'
-    red:        '${red}'
-    green:      '${green}'
-    yellow:     '${yellow}'
-    blue:       '${blue}'
-    magenta:    '${purple}'
-    cyan:       '${cyan}'
-    white:      '${white}'
+[colors.normal]
+black:      '${black}'
+red:        '${red}'
+green:      '${green}'
+yellow:     '${yellow}'
+blue:       '${blue}'
+magenta:    '${purple}'
+cyan:       '${cyan}'
+white:      '${white}'
 
-  bright:
-    black:      '${bright_black}'
-    red:        '${bright_red}'
-    green:      '${bright_green}'
-    yellow:     '${bright_yellow}'
-    blue:       '${bright_blue}'
-    magenta:    '${bright_purple}'
-    cyan:       '${bright_cyan}'
-    white:      '${bright_white}'
+[colors.bright]
+black:      '${bright_black}'
+red:        '${bright_red}'
+green:      '${bright_green}'
+yellow:     '${bright_yellow}'
+blue:       '${bright_blue}'
+magenta:    '${bright_purple}'
+cyan:       '${bright_cyan}'
+white:      '${bright_white}'
 
-  cursor:
-    text: CellBackground
-    cursor: CellForeground # syntax-cursor-color
+[colors.cursor]
+text: 'CellBackground'
+cursor: 'CellForeground' # syntax-cursor-color
 
-  selection:
-    text: CellForeground
-    background: '${gray}'
+[colors.selection]
+text: 'CellForeground'
+background: '${gray}'
 ]]
 
 return M
