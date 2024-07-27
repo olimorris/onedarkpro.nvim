c825a3490dc3f6fb413607ed2602c1280a127756
Oli Morris <olimorris@users.noreply.github.com>
2024-07-27 23:14:12 +0100

    fix(codecompanion): make header purple

--- lua/onedarkpro/highlights/plugins/codecompanion.lua
+++ lua/onedarkpro/highlights/plugins/codecompanion.lua
@@ -7,7 +7,7 @@ function M.groups(theme)
     local config = require("onedarkpro.config").config
 
     return {
-        CodeCompanionChatHeader = { fg = theme.palette.fg, bold = true },
+        CodeCompanionChatHeader = { fg = theme.palette.purple, bold = true },
         CodeCompanionChatSeparator = { fg = theme.palette.gray },
         CodeCompanionChatTool = { fg = theme.palette.bg, bg = theme.palette.green },
         CodeCompanionChatVariable = { fg = theme.palette.bg, bg = theme.palette.blue },
