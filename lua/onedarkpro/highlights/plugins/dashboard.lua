e46388010e92db63519354b50843aa5811f93fae
yuukibarns <yuukibarns@gmail.com>
2024-09-25 12:55:55 +0800

    Add dashboard hi groups.

--- lua/onedarkpro/highlights/plugins/dashboard.lua
+++ lua/onedarkpro/highlights/plugins/dashboard.lua
@@ -5,10 +5,22 @@ local M = {}
 ---@return table
 function M.groups(theme)
     return {
-        -- dashboardHeader = {},
-        dashboardCenter = { fg = theme.palette.purple },
-        dashboardFooter = { fg = theme.palette.blue },
-        -- dashboardShortCut = {},
+        -- General
+        DashboardHeader = { fg = theme.palette.blue },
+        DashboardFooter = { fg = theme.palette.blue },
+        -- Hyper theme
+        DashboardProjectTitle = { fg = theme.palette.cyan },
+        DashboardProjectTitleIcon = { fg = theme.palette.orange },
+        DashboardProjectIcon = { fg = theme.palette.yellow },
+        DashboardMruTitle = { fg = theme.palette.cyan },
+        DashboardMruIcon = { fg = theme.palette.purple },
+        DashboardFiles = { fg = theme.palette.blue },
+        DashboardShortCutIcon = { fg = theme.palette.red },
+        -- Doome theme
+        DashboardDesc = { fg = theme.palette.cyan },
+        DashboardKey = { fg = theme.palette.orange },
+        DashboardIcon = { fg = theme.palette.cyan, bold = true },
+        DashboardShortCut = { fg = theme.palette.cyan },
     }
 end
 
