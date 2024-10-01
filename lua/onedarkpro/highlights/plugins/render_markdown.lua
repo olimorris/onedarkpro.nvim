0e5fce0bcfeee13a159ca8adb0722a73f1fd6360
psymole <32622833+psymole@users.noreply.github.com>
2024-10-01 18:50:35 -0400

    Update render_markdown.lua
    test

--- lua/onedarkpro/highlights/plugins/render_markdown.lua
+++ lua/onedarkpro/highlights/plugins/render_markdown.lua
@@ -13,17 +13,17 @@ function M.groups(theme)
 		    -- RenderMarkdownTableRow = { fg = C.lavender },
         -- Headings
         RenderMarkdownH1 = { fg = theme.palette.red },
-        RenderMarkdownH1Bg = { bg = color.darken('red', 10, 'onedark')" },
+        RenderMarkdownH1Bg = { bg = color.darken('red', 10) },
         RenderMarkdownH2 = { fg = theme.palette.orange },
-        RenderMarkdownH2Bg = { bg = color.darken('orange', 10, 'onedark')" },
+        RenderMarkdownH2Bg = { bg = color.darken('orange', 10) },
         RenderMarkdownH3 = { fg = theme.palette.yellow },
-        RenderMarkdownH3Bg = { bg = color.darken('yellow', 10, 'onedark')" },
+        RenderMarkdownH3Bg = { bg = color.darken('yellow', 10) },
         RenderMarkdownH4 = { fg = theme.palette.green },
-        RenderMarkdownH4Bg = { bg = color.darken('green', 10, 'onedark')" },
+        RenderMarkdownH4Bg = { bg = color.darken('green', 10) },
         RenderMarkdownH5 = { fg = theme.palette.cyan },
-        RenderMarkdownH5Bg = { bg = color.darken('cyan', 10, 'onedark')" },
+        RenderMarkdownH5Bg = { bg = color.darken('cyan', 10) },
         RenderMarkdownH6 = { fg = theme.palette.purple },
-        RenderMarkdownH6Bg = { bg = color.darken('purple', 10, 'onedark')" },
+        RenderMarkdownH6Bg = { bg = color.darken('purple', 10) },
 
     }
 end
