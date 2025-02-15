7ce1475c70261977626f0c6b703750e7afc8d47f
Oli Morris <olimorris@users.noreply.github.com>
2025-02-15 10:56:14 +0000

    feat(snacks): add more picker highlights

--- lua/onedarkpro/highlights/plugins/snacks.lua
+++ lua/onedarkpro/highlights/plugins/snacks.lua
@@ -7,6 +7,8 @@ function M.groups(theme)
     return {
         -- https://github.com/folke/snacks.nvim
         SnacksIndent = { fg = theme.generated.indentline },
+        SnacksPickerDir = { link = "Text" },
+        SnacksPickerBufFlags = { link = "SnacksPickerDir" },
         SnacksIndentScope = { fg = theme.palette.blue },
     }
 end
