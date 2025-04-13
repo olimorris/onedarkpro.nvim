fda7e896b98790474eed58c142d22bccd310dba9
Oli Morris <olimorris@users.noreply.github.com>
2025-04-13 19:50:43 +0100

    feat(snacks): add new snacks hl group

--- lua/onedarkpro/highlights/plugins/snacks.lua
+++ lua/onedarkpro/highlights/plugins/snacks.lua
@@ -7,6 +7,7 @@ function M.groups(theme)
     return {
         -- https://github.com/folke/snacks.nvim
         SnacksIndent = { fg = theme.generated.indentline },
+        SnacksPickerCol = { fg = theme.generated.line_number },
         SnacksPickerDir = { link = "Text" },
         SnacksPickerBufFlags = { link = "SnacksPickerDir" },
         SnacksIndentScope = { fg = theme.palette.blue },
