95fbe5ce903e4300adc059e98fc29dce3449edbc
Metin Yalcinkaya <metinyalcinkaya@hotmail.com.au>
2025-04-16 18:55:42 +1000

    fix(snacks.indent): use theme.rainbow for setting highlights

--- lua/onedarkpro/highlights/plugins/snacks.lua
+++ lua/onedarkpro/highlights/plugins/snacks.lua
@@ -4,22 +4,19 @@ local M = {}
 ---@param theme table
 ---@return table
 function M.groups(theme)
-    return {
+    local highlights = {
         -- https://github.com/folke/snacks.nvim
         SnacksIndent = { fg = theme.generated.indentline },
+        SnacksIndentScope = { fg = theme.palette.blue },
         SnacksPickerCol = { fg = theme.generated.line_number },
         SnacksPickerDir = { link = "Text" },
         SnacksPickerBufFlags = { link = "SnacksPickerDir" },
-        SnacksIndentScope = { fg = theme.palette.blue },
-        SnacksIndentScope1 = { fg = theme.palette.blue },
-        SnacksIndentScope2 = { fg = theme.palette.red },
-        SnacksIndentScope3 = { fg = theme.palette.orange },
-        SnacksIndentScope4 = { fg = theme.palette.yellow },
-        SnacksIndentScope5 = { fg = theme.palette.green },
-        SnacksIndentScope6 = { fg = theme.palette.cyan },
-        SnacksIndentScope7 = { fg = theme.palette.purple },
-        SnacksIndentScope8 = { fg = theme.palette.highlight },
     }
+    for i, color in ipairs(theme.rainbow) do
+        highlights["SnacksIndentScope" .. i] = { fg = color }
+    end
+
+    return highlights
 end
 
 return M
