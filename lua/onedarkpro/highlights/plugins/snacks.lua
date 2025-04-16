2312331645801bbba100bffe131316b0478a196b
Metin Yalcinkaya <metinyalcinkaya@hotmail.com.au>
2025-04-16 16:50:39 +1000

    feat(snacks.indent): Added extra colors to allow for rainbow indent scope colors
    Added the rest of the palette for SnacksIndentScope{1-8}, allowing users to have
    rainbow scope highlighting!
    
    ```lua
    indent = {
      scope = {
        hl = {
          "SnacksIndentScope1",
          "SnacksIndentScope2",
          "SnacksIndentScope3",
          "SnacksIndentScope4",
          "SnacksIndentScope5",
          "SnacksIndentScope6",
          "SnacksIndentScope7",
          "SnacksIndentScope8",
        }
      }
    }
    ```

--- lua/onedarkpro/highlights/plugins/snacks.lua
+++ lua/onedarkpro/highlights/plugins/snacks.lua
@@ -11,6 +11,14 @@ function M.groups(theme)
         SnacksPickerDir = { link = "Text" },
         SnacksPickerBufFlags = { link = "SnacksPickerDir" },
         SnacksIndentScope = { fg = theme.palette.blue },
+        SnacksIndentScope1 = { fg = theme.palette.blue },
+        SnacksIndentScope2 = { fg = theme.palette.red },
+        SnacksIndentScope3 = { fg = theme.palette.orange },
+        SnacksIndentScope4 = { fg = theme.palette.yellow },
+        SnacksIndentScope5 = { fg = theme.palette.green },
+        SnacksIndentScope6 = { fg = theme.palette.cyan },
+        SnacksIndentScope7 = { fg = theme.palette.purple },
+        SnacksIndentScope8 = { fg = theme.palette.highlight },
     }
 end
 
