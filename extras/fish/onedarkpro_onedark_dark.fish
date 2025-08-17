1e44c0ce6f6da00756f2dd4e51c9d959a3798a3b
Oli <oli.morris@hey.com>
2025-08-17 12:53:52 +0100

    update fish extras (#321)
    Co-authored-by: Oli Morris <olimorris@users.noreply.github.com>

--- extras/fish/onedarkpro_onedark_dark.fish
+++ extras/fish/onedarkpro_onedark_dark.fish
@@ -19,11 +19,11 @@ set -g fish_color_redirection $foreground
 set -g fish_color_end $orange
 set -g fish_color_option $yellow
 set -g fish_color_error $red
-set -g fish_color_param $purple
+set -g fish_color_param $blue
 set -g fish_color_comment $comment
 set -g fish_color_selection --background=$selection
 set -g fish_color_search_match --background=$selection
-set -g fish_color_operator $green
+set -g fish_color_operator $foreground
 set -g fish_color_escape $purple
 set -g fish_color_autosuggestion $comment
 set -g fish_color_cwd $cyan
