b8b4e7d89cbc5f8b3538b6175c98c345410e42cb
Aasim-A <23695024+Aasim-A@users.noreply.github.com>
2025-12-11 21:48:46 +1100

    fix: put colors in rainbow order

--- lua/onedarkpro/highlights/plugins/blink_indent.lua
+++ lua/onedarkpro/highlights/plugins/blink_indent.lua
@@ -9,17 +9,17 @@ function M.groups(theme)
         BlinkIndentOrange = { fg = theme.palette.orange },
         BlinkIndentYellow = { fg = theme.palette.yellow },
         BlinkIndentGreen = { fg = theme.palette.green },
-        BlinkIndentViolet = { fg = theme.palette.purple },
         BlinkIndentCyan = { fg = theme.palette.cyan },
         BlinkIndentBlue = { fg = theme.palette.blue },
+        BlinkIndentViolet = { fg = theme.palette.purple },
 
         BlinkIndentRedUnderline = { sp = theme.palette.red, underline = true },
         BlinkIndentOrangeUnderline = { sp = theme.palette.orange, underline = true },
         BlinkIndentYellowUnderline = { sp = theme.palette.yellow, underline = true },
         BlinkIndentGreenUnderline = { sp = theme.palette.green, underline = true },
-        BlinkIndentVioletUnderline = { sp = theme.palette.purple, underline = true },
         BlinkIndentCyanUnderline = { sp = theme.palette.cyan, underline = true },
         BlinkIndentBlueUnderline = { sp = theme.palette.blue, underline = true },
+        BlinkIndentVioletUnderline = { sp = theme.palette.purple, underline = true },
     }
 end
 
