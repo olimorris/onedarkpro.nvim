6b12e00bf5e509e1278a84e2aecf918ca6be9aea
Oli <oli.morris@hey.com>
2025-12-14 23:54:46 +0000

    chore: fix formatting (#342)
    Co-authored-by: Oli Morris <olimorris@users.noreply.github.com>

--- plugin/onedarkpro.lua
+++ plugin/onedarkpro.lua
@@ -1,8 +1,10 @@
-if vim.g.loaded_onedarkpro then return end
+if vim.g.loaded_onedarkpro then
+  return
+end
 
 local cmds = require("onedarkpro.commands")
 for _, cmd in ipairs(cmds) do
-    vim.api.nvim_create_user_command(cmd.cmd, cmd.callback, cmd.opts)
+  vim.api.nvim_create_user_command(cmd.cmd, cmd.callback, cmd.opts)
 end
 
 vim.g.loaded_onedarkpro = true
